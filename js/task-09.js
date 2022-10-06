95af8262c8c960ad1e332db81b47d5fe018e946b
GraysTania <101881748+GraysTania@users.noreply.github.com>
2022-10-06 17:23:25 +0300

    333

--- js/task-09.js
+++ js/task-09.js
@@ -2,13 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const color = getRandomHexColor();
 const bodyEl = document.querySelector('body');
 const spanEl = document.querySelector('.color');
 const butEl = document.querySelector('.change-color');
 
 butEl.addEventListener('click', onClickButColor);
 function onClickButColor(evt) {
+  const color = getRandomHexColor();
   bodyEl.style.backgroundColor = color;
   spanEl.textContent = color;
 }
